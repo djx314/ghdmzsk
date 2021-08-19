481d4c9142c9f173165a648dc8535d029d0c3b66
djx314 <djx314@sina.cn>
2021-08-19 18:04:19 +0800

    Config.

--- a/a03-滚筒洗衣机模式-误/src/main/scala/a03/Counter.scala
+++ a/a03-滚筒洗衣机模式-误/src/main/scala/a03/Counter.scala
@@ -9,39 +9,47 @@ case object Number1T                           extends Number1
 trait Number2 {
   def method1(number3: Number3, number4: Number4): Number1
 }
-case class Number2S(tail: Number2, head: Item) extends Number2 {
-  override def method1(number3: Number3, number4: Number4): Number1 = number3.method2(tail, number4, head)
+case class Number2S(tail: () => Number2, head: Item) extends Number2 {
+  override def method1(number3: Number3, number4: Number4): Number1 = number3.receive1(tail(), number4, head)
 }
-case object Number2T extends Number2 {
-  override def method1(number3: Number3, number4: Number4): Number1 = Number1T
+case class Number2T(tail: () => Number2) extends Number2 {
+  override def method1(number3: Number3, number4: Number4): Number1 = number3.receive0(tail(), number4)
+}
+object Number2 {
+  lazy val zero: Number2 = Number2T(() => zero)
 }
 
 trait Number3 {
-  def method2(number2: Number2, number4: Number4, item2: Item): Number1
+  def receive0(number2: Number2, number4: Number4): Number1
+  def receive1(number2: Number2, number4: Number4, item1: Item): Number1
 }
-case class Number3S(tail: Number3, head: Item) extends Number3 {
-  override def method2(number2: Number2, number4: Number4, item2: Item): Number1 = number4.method4(number2, tail, item2, head)
+case class Number3S(tail: () => Number3, head: Item) extends Number3 {
+  override def receive0(number2: Number2, number4: Number4): Number1              = number4.receive1(number2, tail(), head)
+  override def receive1(number2: Number2, number4: Number4, item1: Item): Number1 = number4.receive2(number2, tail(), item1, head)
 }
 case class Number3T(tail: () => Number3) extends Number3 {
-  override def method2(number2: Number2, number4: Number4, item2: Item): Number1 = number4.method5(number2, tail(), item2)
+  override def receive0(number2: Number2, number4: Number4): Number1              = Number1T
+  override def receive1(number2: Number2, number4: Number4, item1: Item): Number1 = number4.receive1(number2, tail(), item1)
 }
 object Number3T {
   lazy val zero: Number3 = Number3T(() => zero)
 }
 
 trait Number4 {
-  def method4(number2: Number2, number3: Number3, item2: Item, item3: Item): Number1
-  def method5(number2: Number2, number3: Number3, item2: Item): Number1
+  def receive0(number2: Number2, number3: Number3): Number1
+  def receive1(number2: Number2, number3: Number3, item2: Item): Number1
+  def receive2(number2: Number2, number3: Number3, item1: Item, item2: Item): Number1
 }
-case class Number4S(tail: Number4, head: Item) extends Number4 {
-  override def method4(number2: Number2, number3: Number3, item2: Item, item3: Item): Number1 =
-    Number1S(number2.method1(number3, tail), item2)
-  override def method5(number2: Number2, number3: Number3, item2: Item): Number1 = number2.method1(number3, tail)
+case class Number4S(tail: () => Number4, head: Item) extends Number4 {
+  override def receive0(number2: Number2, number3: Number3): Number1                           = Number1T
+  override def receive1(number2: Number2, number3: Number3, item1: Item): Number1              = number2.method1(number3, tail())
+  override def receive2(number2: Number2, number3: Number3, item1: Item, item2: Item): Number1 = number3.receive1(number2, tail(), item1)
 }
 case class Number4T(tail: () => Number4) extends Number4 {
-  override def method4(number2: Number2, number3: Number3, item2: Item, item3: Item): Number1 =
-    Number1S(Number1S(number2.method1(number3, tail()), item2), item3)
-  override def method5(number2: Number2, number3: Number3, item2: Item): Number1 = Number1S(number2.method1(number3, tail()), item2)
+  override def receive0(number2: Number2, number3: Number3): Number1              = Number1T
+  override def receive1(number2: Number2, number3: Number3, item2: Item): Number1 = Number1S(number2.method1(number3, tail()), item2)
+  override def receive2(number2: Number2, number3: Number3, item1: Item, item2: Item): Number1 =
+    Number1S(number3.receive1(number2, tail(), item1), item2)
 }
 object Number4T {
   lazy val zero: Number4 = Number4T(() => zero)
--- a/a03-滚筒洗衣机模式-误/src/main/scala/a03/Runner.scala
+++ a/a03-滚筒洗衣机模式-误/src/main/scala/a03/Runner.scala
@@ -15,40 +15,87 @@ object Runner {
   val item12 = Item("Item12")
   val item13 = Item("Item13")
 
-  val number1 = Number2S(Number2S(Number2T, item01), item02)
-  val number2 = Number3S(Number3S(Number3S(Number3S(Number3S(Number3T.zero, item03), item04), item05), item06), item07)
-  val number3 = Number4S(Number4S(Number4S(Number4S(Number4S(Number4T.zero, item08), item09), item10), item11), item12)
-  val number4 = Number1S(Number1S(Number1T, item01), item02)
-
-  val number5 = Number2S(Number2S(Number2T, item01), item02)
-  val number6 = Number3S(Number3S(Number3S(Number3S(Number3S(Number3T.zero, item03), item04), item05), item06), item07)
-  val number7 = Number4S(Number4S(Number4S(Number4T.zero, item10), item11), item12)
-  val number8 = Number1S(Number1S(Number1S(Number1S(Number1T, item01), item02), item03), item04)
-
-  /** val number8 = Number2S(Number2S(Number2S(Number2S(Number2T, item01), item02), item03), item04) val number9 =
-    * Number3S(Number3S(Number3S(Number3T, item05), item06), item07) val number10 =
-    * Number4S(Number4S(Number4S(Number4S(Number4S(Number4S(Number4T, item08), item09), item10), item11), item12), item13) val number11 =
-    * Number2S(Number2T, item01)
-    *
-    * val number12 = Number2S(Number2S(Number2S(Number2S(Number2T, item01), item02), item03), item04) val number13 =
-    * Number3S(Number3S(Number3T, item05), item06) val number14 = Number4S(Number4S(Number4S(Number4S(Number4S(Number4S(Number4T, item07),
-    * item08), item09), item10), item11), item12) val number15 = Number2T
-    *
-    * val number16 = Number2S(Number2S(Number2T, item01), item02) val number17 = Number3S(Number3S(Number3T, item03), item04) val number18 =
-    * Number4S(Number4S(Number4S(Number4S(Number4S(Number4S(Number4S(Number4T, item05), item06), item07), item08), item09), item10), item11)
-    * val number19 = Number2T
-    *
-    * val number20 = Number2S(Number2S(Number2T, item01), item02) val number21 = Number3S(Number3S(Number3T, item03), item04) val number22 =
-    * Number4S(Number4S(Number4S(Number4S(Number4T, item05), item06), item07), item08)
-    */
-
   def main(args: Array[String]): Unit = {
-    assert(Counter.count(number1, number2, number3) == number4)
-    println(Counter.count(number5, number6, number7))
-    /*assert(Counter.count(number4, number5, number6) == number7)
-    assert(Counter.count(number8, number9, number10) == number11)
-    assert(Counter.count(number12, number13, number14) == number15)
-    assert(Counter.count(number16, number17, number18) == number19)
-    assert(Counter.count(number20, number21, number22) == Number2T)*/
+    {
+      val number1 = Number2S(() => Number2S(() => Number2.zero, item01), item02)
+      val number2 = Number3S(
+        () => Number3S(() => Number3S(() => Number3S(() => Number3S(() => Number3T.zero, item03), item04), item05), item06),
+        item07
+      )
+      val number3 = Number4S(
+        () => Number4S(() => Number4S(() => Number4S(() => Number4S(() => Number4T.zero, item08), item09), item10), item11),
+        item12
+      )
+      val number4 = Number1S(Number1S(Number1T, item01), item02)
+      assert(Counter.count(number1, number2, number3) == number4)
+    }
+
+    {
+      val number1 = Number2S(() => Number2S(() => Number2.zero, item01), item02)
+      val number2 = Number3S(
+        () => Number3S(() => Number3S(() => Number3S(() => Number3S(() => Number3T.zero, item03), item04), item05), item06),
+        item07
+      )
+      val number3 = Number4S(() => Number4S(() => Number4S(() => Number4T.zero, item10), item11), item12)
+      val number4 = Number1S(Number1S(Number1S(Number1S(Number1T, item01), item02), item03), item04)
+      assert(Counter.count(number1, number2, number3) == number4)
+    }
+
+    {
+      val number1 = Number2S(() => Number2S(() => Number2S(() => Number2S(() => Number2.zero, item01), item02), item03), item04)
+      val number2 = Number3S(() => Number3S(() => Number3S(() => Number3T.zero, item05), item06), item07)
+      val number3 =
+        Number4S(
+          () =>
+            Number4S(
+              () => Number4S(() => Number4S(() => Number4S(() => Number4S(() => Number4T.zero, item08), item09), item10), item11),
+              item12
+            ),
+          item13
+        )
+      val number4 = Number1S(Number1T, item01)
+      assert(Counter.count(number1, number2, number3) == number4)
+    }
+
+    {
+      val number1 = Number2S(() => Number2S(() => Number2S(() => Number2S(() => Number2.zero, item01), item02), item03), item04)
+      val number2 =
+        Number3S(() => Number3S(() => Number3T.zero, item05), item06)
+      val number3 = Number4S(
+        () =>
+          Number4S(
+            () => Number4S(() => Number4S(() => Number4S(() => Number4S(() => Number4T.zero, item07), item08), item09), item10),
+            item11
+          ),
+        item12
+      )
+      assert(Counter.count(number1, number2, number3) == Number1T)
+    }
+
+    {
+      val number1 = Number2S(() => Number2S(() => Number2.zero, item01), item02)
+      val number2 = Number3S(() => Number3S(() => Number3T.zero, item03), item04)
+      val number3 =
+        Number4S(
+          () =>
+            Number4S(
+              () =>
+                Number4S(
+                  () => Number4S(() => Number4S(() => Number4S(() => Number4S(() => Number4T.zero, item05), item06), item07), item08),
+                  item09
+                ),
+              item10
+            ),
+          item11
+        )
+      assert(Counter.count(number1, number2, number3) == Number1T)
+    }
+
+    {
+      val number1 = Number2S(() => Number2S(() => Number2.zero, item01), item02)
+      val number2 = Number3S(() => Number3S(() => Number3T.zero, item03), item04)
+      val number3 = Number4S(() => Number4S(() => Number4S(() => Number4S(() => Number4T.zero, item05), item06), item07), item08)
+      assert(Counter.count(number1, number2, number3) == Number1T)
+    }
   }
 }
