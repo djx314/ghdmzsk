6d09c238b8c82b56475939deceb758bf4facee13
djx314 <djx314@sina.cn>
2021-08-22 13:32:24 +0800

    Commit.

--- d/d01/src/main/scala/d01/Runner.scala
+++ d/d01/src/main/scala/d01/Runner.scala
@@ -1,18 +1,6 @@
 package d01
 
 object Runner {
-  val item01 = Item("01")
-  val item02 = Item("02")
-  val item03 = Item("03")
-  val item04 = Item("04")
-  val item05 = Item("05")
-  val item06 = Item("06")
-  val item07 = Item("07")
-  val item08 = Item("08")
-  val item09 = Item("09")
-  val item10 = Item("10")
-  val item11 = Item("11")
-
   lazy val number2Zero: Number2 = Number2T(() => number2Zero)
   lazy val number5Zero: Number5 = Number5(() => number5Zero)
 
@@ -29,9 +17,23 @@ object Runner {
   def main(args: Array[String]): Unit = {
     {
       val minus = minusFromInt(20)
-      for (i <- 0 to 10000) {
+      for (i <- 0 to 5000) {
         val beMinus = beMinusFromInt(i)
         assert(beMinus.send0(minus).length == (if (i - 20 >= 0) i - 20 else 0))
+      }
+    }
+    {
+      val minus = minusFromInt(0)
+      for (i <- 0 to 5000) {
+        val beMinus = beMinusFromInt(i)
+        assert(beMinus.send0(minus).length == (if (i - 0 >= 0) i - 0 else 0))
+      }
+    }
+    {
+      val minus = minusFromInt(5000)
+      for (i <- 0 to 5000) {
+        val beMinus = beMinusFromInt(i)
+        assert(beMinus.send0(minus).length == 0)
       }
     }
   }
