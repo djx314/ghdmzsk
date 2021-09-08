ffcc26fc88c59b12c0beb6989ebb322f35175bd5
djx314 <djx314@sina.cn>
2021-09-09 02:50:41 +0800

    jysg.

--- d/d03/src/main/scala/aaaaaaaaaaaaaa/Counter.scala
+++ d/d03/src/main/scala/aaaaaaaaaaaaaa/Counter.scala
@@ -23,3 +23,29 @@ case class Number3P(tail: Number2) extends Number3
 trait Result
 case class ResultP(tail: Result) extends Result
 case object ResultO              extends Result
+
+// =========================================================================
+
+trait Num1
+
+case class NumP1(per: () => Num1, tail: () => Num1) extends Num1
+
+case class NumT1(tail: () => Num1) extends Num1
+
+case class NumO1(per: () => Num1) extends Num1
+
+object Ni {
+
+  lazy val n1: Num1 = NumT1(() => n2)
+  lazy val n2: Num1 = NumP1(() => n1, () => n3)
+  lazy val n3: Num1 = NumP1(() => n2, () => n4)
+  lazy val n4: Num1 = NumO1(() => n3)
+
+  // 诸位还记得大明湖畔那个总是不准的乘除吗？
+  // 象棋先走和后走总是没有区别的，因为相差并不足以造成输赢差距
+  // 所以乘除的那对阴阳，如果再微一点，直至导数，会如何？
+  // 无穷小，可否解决现在增长速度等于自身的这个问题？
+  // 因为他需要的，恰恰是导数
+  // 零、微和万，紧密相连，互相独立。
+
+}
