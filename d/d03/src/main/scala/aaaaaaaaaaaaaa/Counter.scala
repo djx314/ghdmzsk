d2d650e842f4765f90092d691ddbf6d03b87510c
djx314 <djx314@sina.cn>
2021-09-12 00:57:01 +0800

    Tail

--- d/d03/src/main/scala/aaaaaaaaaaaaaa/Counter.scala
+++ d/d03/src/main/scala/aaaaaaaaaaaaaa/Counter.scala
@@ -34,7 +34,7 @@ case class NumT1(tail: () => Num1) extends Num1
 
 case class NumO1(per: () => Num1) extends Num1
 
-object Ni {
+object Numi {
 
   lazy val n1: Num1 = NumT1(() => n2)
   lazy val n2: Num1 = NumP1(() => n1, () => n3)
@@ -47,5 +47,25 @@ object Ni {
   // 无穷小，可否解决现在增长速度等于自身的这个问题？
   // 因为他需要的，恰恰是导数
   // 零、微和万，紧密相连，互相独立。
+
+}
+
+// =========================================================================
+
+trait N1
+case class NP1(tail: N1)       extends N1
+case class NO1(tail: () => N1) extends N1
+
+trait N2
+case class NP2(tail: N2)       extends N2
+case class NO2(tail: () => N2) extends N2
+
+object Ni {
+
+  lazy val n1: N1 = NP1(NP1(NP1(NP1(n2))))
+  lazy val n2: N1 = NO1(() => NO1(() => NO1(() => NO1(() => n1))))
+
+  lazy val n3: N2 = NP2(NP2(NP2(NP2(n4))))
+  lazy val n4: N2 = NO2(() => n3)
 
 }
