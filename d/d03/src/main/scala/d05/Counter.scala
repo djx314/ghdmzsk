3cdb8c9e4e31e5cf10ddf9268a82b4676a126630
djx314 <djx314@sina.cn>
2021-09-14 02:23:40 +0800

    Commit.

--- d/d03/src/main/scala/d05/Counter.scala
+++ d/d03/src/main/scala/d05/Counter.scala
@@ -2,32 +2,40 @@ package d05
 
 class Item
 
-trait Number1 {
-  def 右推动(left: Number1): Result                = ResultO
-  def 左推动(right: Number1): Result               = ResultO
-  def 推动(left: Number1, right: Number1): Result = ResultO
+trait NumberRight {
+  def 右推动(left: NumberLeft): Result = ResultO
 }
-case class Number1RightS(tail: Number1, head: Item) extends Number1 {
-  override def 右推动(left: Number1): Result = ResultP(tail.右推动(left), head)
+case class NumberRightS(tail: NumberRight, head: Item) extends NumberRight {
+  override def 右推动(left: NumberLeft): Result = ResultP(tail.右推动(left), head)
 }
-case class Number1RightT(tail: () => Number1) extends Number1 {
-  override def 右推动(left: Number1): Result = left.左推动(tail())
+case class NumberRightT(tail: () => NumberRight) extends NumberRight {
+  override def 右推动(left: NumberLeft): Result = left.左推动(tail())
 }
-case class Number1MiddleS(tail: Number1) extends Number1 {
-  override def 推动(left: Number1, right: Number1): Result = right.右推动(Number1U(left, tail))
+case class NumberRightU(tail: NumberRight, head: NumberMiddle) extends NumberRight {
+  override def 右推动(left: NumberLeft): Result = head.推动(left, tail)
 }
-case class Number1MiddleT(tail: () => Number1) extends Number1 {
-  override def 推动(left: Number1, right: Number1): Result = left.左推动(Number1U(right, tail()))
+
+trait NumberMiddle {
+  def 推动(left: NumberLeft, right: NumberRight): Result = ResultO
 }
-case class Number1LeftS(tail: Number1) extends Number1 {
-  override def 左推动(right: Number1): Result = right.右推动(tail)
+case class NumberMiddleS(tail: NumberMiddle) extends NumberMiddle {
+  override def 推动(left: NumberLeft, right: NumberRight): Result = right.右推动(NumberLeftU(left, tail))
 }
-case object Number1LeftT extends Number1 {
-  override def 左推动(right: Number1): Result = ResultO
+case class NumberMiddleT(tail: () => NumberMiddle) extends NumberMiddle {
+  override def 推动(left: NumberLeft, right: NumberRight): Result = left.左推动(NumberRightU(right, tail()))
 }
-case class Number1U(tail: Number1, head: Number1) extends Number1 {
-  override def 右推动(left: Number1): Result  = head.推动(left, tail)
-  override def 左推动(right: Number1): Result = head.推动(tail, right)
+
+trait NumberLeft {
+  def 左推动(right: NumberRight): Result = ResultO
+}
+case class NumberLeftS(tail: NumberLeft) extends NumberLeft {
+  override def 左推动(right: NumberRight): Result = right.右推动(tail)
+}
+case object NumberLeftT extends NumberLeft {
+  override def 左推动(right: NumberRight): Result = ResultO
+}
+case class NumberLeftU(tail: NumberLeft, head: NumberMiddle) extends NumberLeft {
+  override def 左推动(right: NumberRight): Result = head.推动(tail, right)
 }
 
 trait Result
