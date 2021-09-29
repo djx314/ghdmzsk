3c0b9f2b3eae51e98b663161173395ffeb4b6dfc
djx314 <djx314@sina.cn>
2021-09-30 02:21:23 +0800

    减法。

--- a/a02/src/main/scala/a02/Counter.scala
+++ a/a02/src/main/scala/a02/Counter.scala
@@ -3,21 +3,21 @@ package a02
 case class Item(name: String)
 
 trait Number1 {
-  def method1(number2: Number2): Number2
+  def method1(number2: Number2, item2: Item): Number1
 }
 case class Number1S(tail: Number1, head: Item) extends Number1 {
-  override def method1(number2: Number2): Number2 = number2.method2(tail, head)
+  override def method1(number2: Number2, item2: Item): Number1 = number2.method2(tail)
 }
 case object Number1T extends Number1 {
-  override def method1(number2: Number2): Number2 = number2
+  override def method1(number2: Number2, item2: Item): Number1 = Number1T
 }
 
 trait Number2 {
-  def method2(number1: Number1, item1: Item): Number2
+  def method2(number1: Number1): Number1
 }
 case class Number2S(tail: Number2, head: Item) extends Number2 {
-  override def method2(number1: Number1, item1: Item): Number2 = number1.method1(tail)
+  override def method2(number1: Number1): Number1 = number1.method1(tail, head)
 }
 case object Number2T extends Number2 {
-  override def method2(number1: Number1, item1: Item): Number2 = Number2T
+  override def method2(number1: Number1): Number1 = number1
 }
