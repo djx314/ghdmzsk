1a32d0e7877ddab5afabfc0b9e7095e33b69c0ca
djx314 <djx314@sina.cn>
2021-11-04 22:31:03 +0800

    M

---
+++ a/a02-02/build.sbt
@@ -0,0 +1,5 @@
+Settings.settings
+name := "a02-02"
+
+libraryDependencies ++= Dependent.catsEffect
+libraryDependencies ++= Dependent.sttp
---
+++ a/a02-02/src/main/scala/a02_02/Runner.scala
@@ -0,0 +1,24 @@
+package a02_02
+
+import cats.effect._
+import cats.effect.unsafe.implicits.global
+import sttp.client3._
+
+object Runner {
+
+  def main(args: Array[String]): Unit = {
+    {
+      val uri     = uri"http://www.baidu.com"
+      val result1 = Base.requestAction(uri)
+      val result2 = Compare.request(uri)
+      val compareAction = for {
+        r1 <- result1
+        r2 <- result2
+      } yield (r1.contains("百度"), r2.contains("百度"))
+      val compareResult = compareAction.unsafeRunSync()
+      assert(compareResult._1 == true)
+      assert(compareResult._2 == true)
+    }
+  }
+
+}
