fe8cc90bda9f4180efc63ac5b150108cda182210
djx314 <djx314@sina.cn>
2021-11-18 05:45:00 +0800

    11

--- e/e04/src/main/scala/e04/Runner.scala
+++ e/e04/src/main/scala/e04/Runner.scala
@@ -3,14 +3,12 @@ package e04
 import e01._
 
 object Runner {
-  def numberFromInt[T](n: Int, item: T): (Number[T], NumberT[T]) = {
-    def gen(n1: Int, zero: => Number[T]): Number[T] = n1 match {
-      case n2 if n2 > 0 => NumberS(() => gen(n2 - 1, zero), item)
-      case 0            => zero
-    }
-    lazy val num1: Number[T]  = gen(n, num2)
-    lazy val num2: NumberT[T] = NumberT(() => num1)
+  def numberFromInt[T](n: Int, item: T,zero: => Number[T]): Number[T] = {
+    def gen(n1: Int, zero: => Number[T]): Number[T] = if (n1 > 0) NumberS(() => gen(n1 - 1, zero), item) else zero
+    lazy val num1: Number[T]                        = gen(n, num2)
+    lazy val num2: NumberT[T]                       = NumberT(() => num1)
     (num1, num2)
+    if (n > 0) NumberS(() => numberFromInt(n - 1,item, zero), item) else zero
   }
 
   def countNumber[T](num: Collect[T]): Int = num match {
@@ -20,10 +18,18 @@ object Runner {
 
   def main(args: Array[String]): Unit = {
     def numFromDi(di: Int): Collect[Item4] = {
-      val (col11, col12) = numberFromInt(di, new Item1)
-      val (col21, col22) = numberFromInt(di, new Item2)
-      val (col31, col32) = numberFromInt(di, new Item3)
-      val (col41, col42) = numberFromInt(di, new Item4)
+      lazy val col11 :Number[Item1]= numberFromInt(di, new Item1,col12)
+      lazy val col12 :Number[Item1]= NumberT(() => col11)
+
+      lazy val col21 :Number[Item2]= numberFromInt(di, new Item2,col22)
+      lazy val col22 :Number[Item2]= NumberT(() => col21)
+
+      lazy val col31 :Number[Item3]= numberFromInt(di, new Item3,col32)
+      lazy val col32 :Number[Item3]= NumberT(() => col31)
+
+      lazy val col41 :Number[Item4]= numberFromInt(di, new Item4,col42)
+      lazy val col42 :Number[Item4]= NumberT(() => col41)
+
       col11.execute(new HList1Context)((), HPositive(col21, HPositive(col31, HPositive(col41, HNil()))))
     }
     {
