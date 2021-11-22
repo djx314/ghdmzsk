811926a60f745157665ef917eb2da412fac3ad1a
djx314 <djx314@sina.cn>
2021-11-23 00:51:53 +0800

    11

--- e/e01-02/src/main/scala/e01_02/ops/Ops1.scala
+++ e/e01-02/src/main/scala/e01_02/ops/Ops1.scala
@@ -51,4 +51,77 @@ object Ops1 {
     override def bindT(number: Number[A], parameter: A1): Boolean = false
   }
 
+  class Ops_Corresponds1[A, B] extends TypeContext {
+    override type toDataType = Number[B]
+    override type Parameter  = (A, B) => Boolean
+    override type Result     = Boolean
+  }
+
+  def ops_corresponds1[A, B]: Context[Ops_Corresponds1[A, B], A] = new Context[Ops_Corresponds1[A, B], A] {
+    override type DataCtx = (Number[A], Number[B])
+    override def convertS(t: Number[B], current: () => Number[A]): (Number[A], Number[B]) = (current(), t)
+    override def convertT(t: Number[B], current: () => Number[A]): (Number[A], Number[B]) = (current(), t)
+    override def bindS(number: (Number[A], Number[B]), parameter: (A, B) => Boolean, head: A): Boolean =
+      number._2.execute(ops_corresponds2[A, B])((parameter, head), number._1)
+    override def bindT(number: (Number[A], Number[B]), parameter: (A, B) => Boolean): Boolean = number._2.execute(ops_corresponds3)((), ())
+  }
+
+  class Ops_Corresponds2[A, B] extends TypeContext {
+    override type toDataType = Number[A]
+    override type Parameter  = ((A, B) => Boolean, A)
+    override type Result     = Boolean
+  }
+
+  def ops_corresponds2[A, B]: Context[Ops_Corresponds2[A, B], B] = new Context[Ops_Corresponds2[A, B], B] {
+    override type DataCtx = (Number[A], Number[B])
+    override def convertS(t: Number[A], current: () => Number[B]): (Number[A], Number[B]) = (t, current())
+    override def convertT(t: Number[A], current: () => Number[B]): (Number[A], Number[B]) = (t, current())
+    override def bindS(number: (Number[A], Number[B]), parameter: ((A, B) => Boolean, A), head: B): Boolean =
+      if (parameter._1(parameter._2, head)) number._1.execute(ops_corresponds1[A, B])(parameter._1, number._2)
+      else false
+    override def bindT(number: (Number[A], Number[B]), parameter: ((A, B) => Boolean, A)): Boolean = false
+  }
+
+  class Ops_Corresponds3 extends TypeContext {
+    override type toDataType = Unit
+    override type Parameter  = Unit
+    override type Result     = Boolean
+  }
+
+  def ops_corresponds3[B]: Context[Ops_Corresponds3, B] = new Context[Ops_Corresponds3, B] {
+    override type DataCtx = Unit
+    override def convertS(t: Unit, current: () => Number[B]): Unit      = ()
+    override def convertT(t: Unit, current: () => Number[B]): Unit      = ()
+    override def bindS(number: Unit, parameter: Unit, head: B): Boolean = false
+    override def bindT(number: Unit, parameter: Unit): Boolean          = true
+  }
+
+  class HeadOptionTypeContext[A] extends TypeContext {
+    override type toDataType = Unit
+    override type Parameter  = Unit
+    override type Result     = Option[A]
+  }
+
+  def ops_headOption[A]: Context[HeadOptionTypeContext[A], A] = new Context[HeadOptionTypeContext[A], A] {
+    override type DataCtx = () => Number[A]
+    override def convertS(t: Unit, current: () => Number[A]): () => Number[A]        = current
+    override def convertT(t: Unit, current: () => Number[A]): () => Number[A]        = current
+    override def bindS(number: () => Number[A], parameter: Unit, head: A): Option[A] = Option(head)
+    override def bindT(number: () => Number[A], parameter: Unit): Option[A]          = Option.empty
+  }
+
+  class HeadTypeContext[A] extends TypeContext {
+    override type toDataType = Unit
+    override type Parameter  = Unit
+    override type Result     = A
+  }
+
+  def ops_head[A]: Context[HeadTypeContext[A], A] = new Context[HeadTypeContext[A], A] {
+    override type DataCtx = () => Number[A]
+    override def convertS(t: Unit, current: () => Number[A]): () => Number[A] = current
+    override def convertT(t: Unit, current: () => Number[A]): () => Number[A] = current
+    override def bindS(number: () => Number[A], parameter: Unit, head: A): A  = head
+    override def bindT(number: () => Number[A], parameter: Unit): A           = throw new NoSuchElementException("head of empty list")
+  }
+
 }
--- e/e01-02/src/test/scala/e01_02/test/NumberTest.scala
+++ e/e01-02/src/test/scala/e01_02/test/NumberTest.scala
@@ -9,6 +9,7 @@ import e01_02._
 
 import e01_02._
 import NumberOps._
+import scala.util.Try
 
 object HelloWorldSpec extends DefaultRunnableSpec {
   def spec = suite("Number-List Spec")(
@@ -31,6 +32,88 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         val result2 = number1 ::: number3
         val equals1 = assert(OpsConvertList.toList(result1))(equalTo(list1 ::: list2))
         val equals2 = assert(OpsConvertList.toList(result2))(equalTo(list1 ::: list3))
+        equals1 && equals2
+      }
+    },
+    test("appendAll") {
+      check(Gen.listOf(Gen.int), Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2, list3) =>
+        val number1 = OpsConvertList.fromList(list1)
+        val number2 = OpsConvertList.fromList(list2)
+        val number3 = OpsConvertList.fromList(list3)
+        val result1 = number1.appendAll(number2)
+        val result2 = number1.appendAll(number3)
+        val equals1 = assert(OpsConvertList.toList(result1))(equalTo(list1.appendedAll(list2)))
+        val equals2 = assert(OpsConvertList.toList(result2))(equalTo(list1.appendedAll(list3)))
+        equals1 && equals2
+      }
+    },
+    test("collect") {
+      check(Gen.listOf(Gen.option(Gen.int)), Gen.listOf(Gen.string)) { (list1, list2) =>
+        val number1 = OpsConvertList.fromList(list1)
+        val number2 = OpsConvertList.fromList(list2)
+        val result1 = number1.collect { case Some(s) => s }
+        val result2 = number1.collect { case s if s.size > 2 => s }
+        val equals1 = assert(OpsConvertList.toList(result1))(equalTo(list1.collect { case Some(s) => s }))
+        val equals2 = assert(OpsConvertList.toList(result2))(equalTo(list1.collect { case s if s.size > 2 => s }))
+        equals1 && equals2
+      }
+    },
+    test("contains") {
+      check(Gen.listOf(Gen.int), Gen.int, Gen.listOf(Gen.string), Gen.string) { (list1, data1, list2, data2) =>
+        val number1 = OpsConvertList.fromList(list1)
+        val number2 = OpsConvertList.fromList(list2)
+        val result1 = number1.contains(data1)
+        val result2 = number1.contains(data2)
+        val result3 = number2.contains(data1)
+        val result4 = number2.contains(data2)
+        val result5 = number1.headOption.map(s => number1.contains(s))
+        val result6 = number2.headOption.map(s => number2.contains(s))
+        val equals1 = assert(result1)(equalTo(list1.contains(data1)))
+        val equals2 = assert(result2)(equalTo(list1.contains(data2)))
+        val equals3 = assert(result3)(equalTo(list2.contains(data1)))
+        val equals4 = assert(result4)(equalTo(list2.contains(data2)))
+        val equals5 = assert(result5)(equalTo(list1.headOption.map(s => list1.contains(s))))
+        val equals6 = assert(result6)(equalTo(list2.headOption.map(s => list2.contains(s))))
+        equals1 && equals2 && equals3 && equals4 && equals5 && equals6
+      }
+    },
+    test("corresponds") {
+      check(Gen.listOf(Gen.int), Gen.listOf(Gen.int), Gen.listOf(Gen.string), Gen.listOf(Gen.string)) { (list1, list2, list3, list4) =>
+        val number1 = OpsConvertList.fromList(list1)
+        val number2 = OpsConvertList.fromList(list2)
+        val number3 = OpsConvertList.fromList(list3)
+        val number4 = OpsConvertList.fromList(list4)
+        val result1 = number1.corresponds(number2)((a, b) => a * b >= 0)
+        val result2 = number3.corresponds(number4)((a, b) => a.size + b.size < 20)
+        val result3 = number1.corresponds(number1)((a, b) => a == b)
+        val result4 = number3.corresponds(number3)(_ == _)
+        val equals1 = assert(result1)(equalTo(list1.corresponds(list2)((a, b) => a * b >= 0)))
+        val equals2 = assert(result2)(equalTo(list3.corresponds(list4)((a, b) => a.size + b.size < 20)))
+        val equals3 = assert(result3)(equalTo(true))
+        val equals4 = assert(result4)(equalTo(true))
+        equals1 && equals2 && equals3 && equals4
+      }
+    },
+    test("headOption") {
+      check(Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2) =>
+        val number1 = OpsConvertList.fromList(list1)
+        val number2 = OpsConvertList.fromList(list2)
+        val result1 = number1.headOption
+        val result2 = number2.headOption
+        val equals1 = assert(result1)(equalTo(list1.headOption))
+        val equals2 = assert(result2)(equalTo(list2.headOption))
+        equals1 && equals2
+      }
+    },
+    test("head") {
+      check(Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2) =>
+        val number1                                            = OpsConvertList.fromList(list1)
+        val number2                                            = OpsConvertList.fromList(list2)
+        def convert[T](t: => T): Either[(Class[_], String), T] = Try(t).toEither.left.map(s => (s.getClass(), s.getMessage()))
+        val result1                                            = convert(number1.head)
+        val result2                                            = convert(number2.head)
+        val equals1                                            = assert(result1)(equalTo(convert(list1.head)))
+        val equals2                                            = assert(result2)(equalTo(convert(list2.head)))
         equals1 && equals2
       }
     }
