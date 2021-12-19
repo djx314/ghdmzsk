1f7c3b2241824ff9c588976f11199595560c4d7b
djx314 <djx314@sina.cn>
2021-12-20 00:17:34 +0800

    sfaserwefrerwrwerwerwer

--- f/f02/src/main/scala/f02/Runner.scala
+++ f/f02/src/main/scala/f02/Runner.scala
@@ -12,7 +12,11 @@ import f02.number1s_top.{
   Number1S_Number1U_Number1S_Top,
   Number1S_Number1U_Number1T_Top,
   Number1S_Number1U_Number1U_Top,
-  Number1S_Number1U_Number1V_Top
+  Number1S_Number1U_Number1V_Top,
+  Number1S_Number1V_Number1S_Top,
+  Number1S_Number1V_Number1T_Top,
+  Number1S_Number1V_Number1U_Top,
+  Number1S_Number1V_Number1V_Top
 }
 
 object Runner {
@@ -29,7 +33,11 @@ object Runner {
     // Number1S_Number1U_Number1S_Top.exe
     // Number1S_Number1U_Number1T_Top.exe
     // Number1S_Number1U_Number1U_Top.exe
-    Number1S_Number1U_Number1V_Top.exe
+    // Number1S_Number1U_Number1V_Top.exe
+    // Number1S_Number1V_Number1S_Top.exe
+    // Number1S_Number1V_Number1T_Top.exe
+    Number1S_Number1V_Number1U_Top.exe
+    Number1S_Number1V_Number1V_Top.exe
   }
 
 }
