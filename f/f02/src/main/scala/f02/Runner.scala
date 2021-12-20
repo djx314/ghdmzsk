be7561a6a55aed466d7710f8ff5c2b6a2421bdad
djx314 <djx314@sina.cn>
2021-12-20 23:34:57 +0800

    sfaserwefrerwrwerwerwer

--- f/f02/src/main/scala/f02/Runner.scala
+++ f/f02/src/main/scala/f02/Runner.scala
@@ -36,8 +36,8 @@ object Runner {
     // Number1S_Number1U_Number1V_Top.exe
     // Number1S_Number1V_Number1S_Top.exe
     // Number1S_Number1V_Number1T_Top.exe
-    Number1S_Number1V_Number1U_Top.exe
-    Number1S_Number1V_Number1V_Top.exe
+    // Number1S_Number1V_Number1U_Top.exe
+    // Number1S_Number1V_Number1V_Top.exe
   }
 
 }
