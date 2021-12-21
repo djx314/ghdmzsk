3525c39ed56139b061c28b4577cad74e746a2f58
djx314 <djx314@sina.cn>
2021-12-21 23:13:02 +0800

    sfaserwefrerwrwerwerwer

--- f/f02/src/main/scala/f02/Runner.scala
+++ f/f02/src/main/scala/f02/Runner.scala
@@ -18,26 +18,40 @@ import f02.number1s_top.{
   Number1S_Number1V_Number1U_Top,
   Number1S_Number1V_Number1V_Top
 }
+import f02.number1t_top.{
+  Number1T_Number1S_Number1S_Top,
+  Number1T_Number1S_Number1T_Top,
+  Number1T_Number1S_Number1U_Top,
+  Number1T_Number1S_Number1V_Top
+}
 
 object Runner {
 
   def main(arr: Array[String]): Unit = {
-    // Number1S_Number1S_Number1S_Top.exe
-    // Number1S_Number1S_Number1T_Top.exe
-    // Number1S_Number1S_Number1U_Top.exe
-    // Number1S_Number1S_Number1V_Top.exe
-    // Number1S_Number1T_Number1S_Top.exe
-    // Number1S_Number1T_Number1T_Top.exe
-    // Number1S_Number1T_Number1U_Top.exe
-    // Number1S_Number1T_Number1V_Top.exe
-    // Number1S_Number1U_Number1S_Top.exe
-    // Number1S_Number1U_Number1T_Top.exe
-    // Number1S_Number1U_Number1U_Top.exe
-    // Number1S_Number1U_Number1V_Top.exe
-    // Number1S_Number1V_Number1S_Top.exe
-    // Number1S_Number1V_Number1T_Top.exe
-    // Number1S_Number1V_Number1U_Top.exe
-    // Number1S_Number1V_Number1V_Top.exe
+    {
+      // Number1S_Number1S_Number1S_Top.exe
+      // Number1S_Number1S_Number1T_Top.exe
+      // Number1S_Number1S_Number1U_Top.exe
+      // Number1S_Number1S_Number1V_Top.exe
+      // Number1S_Number1T_Number1S_Top.exe
+      // Number1S_Number1T_Number1T_Top.exe
+      // Number1S_Number1T_Number1U_Top.exe
+      // Number1S_Number1T_Number1V_Top.exe
+      // Number1S_Number1U_Number1S_Top.exe
+      // Number1S_Number1U_Number1T_Top.exe
+      // Number1S_Number1U_Number1U_Top.exe
+      // Number1S_Number1U_Number1V_Top.exe
+      // Number1S_Number1V_Number1S_Top.exe
+      // Number1S_Number1V_Number1T_Top.exe
+      // Number1S_Number1V_Number1U_Top.exe
+      // Number1S_Number1V_Number1V_Top.exe
+    }
+    {
+      // Number1T_Number1S_Number1S_Top.exe
+      // Number1T_Number1S_Number1T_Top.exe
+      Number1T_Number1S_Number1U_Top.exe
+      Number1T_Number1S_Number1V_Top.exe
+    }
   }
 
 }
