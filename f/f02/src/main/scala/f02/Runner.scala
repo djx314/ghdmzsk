131cd542b669cae4ebc42b297873d2c3aa69dc34
djx314 <djx314@sina.cn>
2022-01-12 23:12:17 +0800

    ax

--- f/f02/src/main/scala/f02/Runner.scala
+++ f/f02/src/main/scala/f02/Runner.scala
@@ -33,8 +33,10 @@ import f02.number1t_top.{
   Number1T_Number1U_Number1V_Top,
   Number1T_Number1V_Number1S_Top,
   Number1T_Number1V_Number1T_Top,
-  Number1T_Number1V_Number1U_Top
+  Number1T_Number1V_Number1U_Top,
+  Number1T_Number1V_Number1V_Top
 }
+import f02.number1u_top.Number1U_Number1S_Number1S_Top
 import f02.number1v_top.Number1V_Number1T_Number1V_Top
 
 object Runner {
@@ -73,7 +75,11 @@ object Runner {
       // Number1T_Number1U_Number1V_Top.exe
       // Number1T_Number1V_Number1S_Top.exe
       // Number1T_Number1V_Number1T_Top.exe
-      Number1T_Number1V_Number1U_Top.exe
+      // Number1T_Number1V_Number1U_Top.exe
+      // Number1T_Number1V_Number1V_Top.exe
+    }
+    {
+      Number1U_Number1S_Number1S_Top.exe
     }
     {
       // Number1V_Number1T_Number1V_Top.exe
