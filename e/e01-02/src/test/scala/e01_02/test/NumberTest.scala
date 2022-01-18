4d83051549639356397c0149dff8c974ae3a4df6
djx314 <djx314@sina.cn>
2022-01-19 02:11:40 +0800

    njidnfjodrngjietbneribterbyer

--- e/e01-02/src/test/scala/e01_02/test/NumberTest.scala
+++ e/e01-02/src/test/scala/e01_02/test/NumberTest.scala
@@ -1,20 +1,22 @@
 package e01_02.test
 
 import zio._
-import zio.Console._
 import zio.test._
 import zio.test.Assertion._
-import zio.test.testEnvironment._
+import zio.test.environment.testEnvironment._
 import e01_02._
 
-import e01_02._
 import NumberOps._
 import scala.util.Try
 
 object HelloWorldSpec extends DefaultRunnableSpec {
   def spec = suite("Number-List Spec")(
-    test("::") {
-      check(Gen.listOf(Gen.int), Gen.int, Gen.string) { (list, item1, item2) =>
+    testM("::") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.int(Int.MinValue, Int.MaxValue),
+        Gen.string(Gen.char(Char.MinValue, Char.MaxValue))
+      ) { (list, item1, item2) =>
         val number1 = OpsConvertList.fromList(list)
         val result1 = item1 :: number1
         val result2 = item2 :: number1
@@ -23,8 +25,12 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         equals1 && equals2
       }
     },
-    test(":::") {
-      check(Gen.listOf(Gen.int), Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2, list3) =>
+    testM(":::") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))
+      ) { (list1, list2, list3) =>
         val number1 = OpsConvertList.fromList(list1)
         val number2 = OpsConvertList.fromList(list2)
         val number3 = OpsConvertList.fromList(list3)
@@ -35,8 +41,12 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         equals1 && equals2
       }
     },
-    test("appendAll") {
-      check(Gen.listOf(Gen.int), Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2, list3) =>
+    testM("appendAll") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))
+      ) { (list1, list2, list3) =>
         val number1 = OpsConvertList.fromList(list1)
         val number2 = OpsConvertList.fromList(list2)
         val number3 = OpsConvertList.fromList(list3)
@@ -47,19 +57,25 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         equals1 && equals2
       }
     },
-    test("collect") {
-      check(Gen.listOf(Gen.option(Gen.int)), Gen.listOf(Gen.string)) { (list1, list2) =>
-        val number1 = OpsConvertList.fromList(list1)
-        val number2 = OpsConvertList.fromList(list2)
-        val result1 = number1.collect { case Some(s) => s }
-        val result2 = number1.collect { case s if s.size > 2 => s }
-        val equals1 = assert(OpsConvertList.toList(result1))(equalTo(list1.collect { case Some(s) => s }))
-        val equals2 = assert(OpsConvertList.toList(result2))(equalTo(list1.collect { case s if s.size > 2 => s }))
-        equals1 && equals2
+    testM("collect") {
+      check(Gen.listOf(Gen.option(Gen.int(Int.MinValue, Int.MaxValue))), Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))) {
+        (list1, list2) =>
+          val number1 = OpsConvertList.fromList(list1)
+          val number2 = OpsConvertList.fromList(list2)
+          val result1 = number1.collect { case Some(s) => s }
+          val result2 = number1.collect { case s if s.size > 2 => s }
+          val equals1 = assert(OpsConvertList.toList(result1))(equalTo(list1.collect { case Some(s) => s }))
+          val equals2 = assert(OpsConvertList.toList(result2))(equalTo(list1.collect { case s if s.size > 2 => s }))
+          equals1 && equals2
       }
     },
-    test("contains") {
-      check(Gen.listOf(Gen.int), Gen.int, Gen.listOf(Gen.string), Gen.string) { (list1, data1, list2, data2) =>
+    testM("contains") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.int(Int.MinValue, Int.MaxValue),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue))),
+        Gen.string(Gen.char(Char.MinValue, Char.MaxValue))
+      ) { (list1, data1, list2, data2) =>
         val number1 = OpsConvertList.fromList(list1)
         val number2 = OpsConvertList.fromList(list2)
         val result1 = number1.contains(data1)
@@ -77,8 +93,13 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         equals1 && equals2 && equals3 && equals4 && equals5 && equals6
       }
     },
-    test("corresponds") {
-      check(Gen.listOf(Gen.int), Gen.listOf(Gen.int), Gen.listOf(Gen.string), Gen.listOf(Gen.string)) { (list1, list2, list3, list4) =>
+    testM("corresponds") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue))),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))
+      ) { (list1, list2, list3, list4) =>
         val number1 = OpsConvertList.fromList(list1)
         val number2 = OpsConvertList.fromList(list2)
         val number3 = OpsConvertList.fromList(list3)
@@ -94,31 +115,37 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         equals1 && equals2 && equals3 && equals4
       }
     },
-    test("headOption") {
-      check(Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2) =>
-        val number1 = OpsConvertList.fromList(list1)
-        val number2 = OpsConvertList.fromList(list2)
-        val result1 = number1.headOption
-        val result2 = number2.headOption
-        val equals1 = assert(result1)(equalTo(list1.headOption))
-        val equals2 = assert(result2)(equalTo(list2.headOption))
-        equals1 && equals2
+    testM("headOption") {
+      check(Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)), Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))) {
+        (list1, list2) =>
+          val number1 = OpsConvertList.fromList(list1)
+          val number2 = OpsConvertList.fromList(list2)
+          val result1 = number1.headOption
+          val result2 = number2.headOption
+          val equals1 = assert(result1)(equalTo(list1.headOption))
+          val equals2 = assert(result2)(equalTo(list2.headOption))
+          equals1 && equals2
       }
     },
-    test("head") {
-      check(Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1, list2) =>
-        val number1                                            = OpsConvertList.fromList(list1)
-        val number2                                            = OpsConvertList.fromList(list2)
-        def convert[T](t: => T): Either[(Class[_], String), T] = Try(t).toEither.left.map(s => (s.getClass(), s.getMessage()))
-        val result1                                            = convert(number1.head)
-        val result2                                            = convert(number2.head)
-        val equals1                                            = assert(result1)(equalTo(convert(list1.head)))
-        val equals2                                            = assert(result2)(equalTo(convert(list2.head)))
-        equals1 && equals2
+    testM("head") {
+      check(Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)), Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))) {
+        (list1, list2) =>
+          val number1                                            = OpsConvertList.fromList(list1)
+          val number2                                            = OpsConvertList.fromList(list2)
+          def convert[T](t: => T): Either[(Class[_], String), T] = Try(t).toEither.left.map(s => (s.getClass(), s.getMessage()))
+          val result1                                            = convert(number1.head)
+          val result2                                            = convert(number2.head)
+          val equals1                                            = assert(result1)(equalTo(convert(list1.head)))
+          val equals2                                            = assert(result2)(equalTo(convert(list2.head)))
+          equals1 && equals2
       }
     },
-    test("equals") {
-      check(Gen.listOf(Gen.int), Gen.listOf(Gen.int), Gen.listOf(Gen.string)) { (list1_1, list1_2, list2) =>
+    testM("equals") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue)))
+      ) { (list1_1, list1_2, list2) =>
         val number1_1 = OpsConvertList.fromList(list1_1)
         val number1_2 = OpsConvertList.fromList(list1_2)
         val number2   = OpsConvertList.fromList(list2)
@@ -135,8 +162,13 @@ object HelloWorldSpec extends DefaultRunnableSpec {
         equals1 && equals2 && equals3 && equals4 && equals5
       }
     },
-    test("exists") {
-      check(Gen.listOf(Gen.int), Gen.int, Gen.listOf(Gen.string), Gen.string) { (list1, data1, list2, data2) =>
+    testM("exists") {
+      check(
+        Gen.listOf(Gen.int(Int.MinValue, Int.MaxValue)),
+        Gen.int(Int.MinValue, Int.MaxValue),
+        Gen.listOf(Gen.string(Gen.char(Char.MinValue, Char.MaxValue))),
+        Gen.string(Gen.char(Char.MinValue, Char.MaxValue))
+      ) { (list1, data1, list2, data2) =>
         val number1 = OpsConvertList.fromList(list1)
         val number2 = OpsConvertList.fromList(list2)
         val result1 = number1.exists(_ > 30)
