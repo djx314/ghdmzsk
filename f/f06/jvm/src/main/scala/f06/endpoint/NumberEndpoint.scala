721d8b429c25d0c8a129972f71d8574c935999fd
djx314 <djx314@sina.cn>
2022-01-22 04:34:50 +0800

    接口比肉体nerotnnt5n4人与人同依然头人

--- f/f06/jvm/src/main/scala/f06/endpoint/NumberEndpoint.scala
+++ f/f06/jvm/src/main/scala/f06/endpoint/NumberEndpoint.scala
@@ -8,9 +8,6 @@ class NumberEndpoint extends NumberEndpointPre {
 
   private val root = endpoint
 
-  private val htmlDocTag = "Html 展示页"
-  private val JsonTag    = "Json 请求"
-
   val pageHelper          = root.in("help").out(htmlBodyUtf8).description("常用页面引导").tag(htmlDocTag)
   val index               = root.in("page" / "index").out(htmlBodyUtf8).description("首页").tag(htmlDocTag)
   val countPlanReviewPage = root.in("page" / "countPlanReviewPage").out(htmlBodyUtf8).description("计算计划统计").tag(htmlDocTag)
