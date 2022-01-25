edbe007c7bc17208309446e25d2bf52244a765b7
djx314 <djx314@sina.cn>
2022-01-26 00:43:54 +0800

    接口比肉体nerotnnt5n4人与人同依然头人

--- f/f03/src/main/scala/f03/service/CodegenService.scala
+++ f/f03/src/main/scala/f03/service/CodegenService.scala
@@ -29,20 +29,24 @@ class CodegenServiceImpl(db: SlickDB, dataCollection: DataCollection) extends Co
     val planAndSet = db.run(getIdAction.result)
 
     def printlnSet(set: Seq[CountSetRow]): CTask[Unit] = {
-      val str   = set.map(s => s"CountSet(firstStart = ${s.firstStart}, secondStart = ${s.secondStart}, set = \"${s.countSet}\")")
+      val str1 = set.map(s =>
+        s"val countSet${s.id}: CountSet = CountSet(firstStart = ${s.firstStart}, secondStart = ${s.secondStart}, set = \"${s.countSet}\")"
+      )
+      val str2  = "package f07" :: "object CountSets {" :: str1.to(List).map(s => "  " + s).appended("}")
       val path  = Paths.get("..", "f07", "src", "main", "codegen", "f07")
       val path1 = path.resolve("CountSets.scala")
       for {
         _         <- blocking.effectBlocking(Files.createDirectories(path))
         printlner <- ZIO.effect(ZManaged.fromAutoCloseable(ZIO.effect(new PrintWriter(path1.toFile))))
-        s         <- printlner.use(p => blocking.effectBlocking(str.foreach(p.println)))
+        s         <- printlner.use(p => blocking.effectBlocking(str2.foreach(p.println)))
       } yield s
     }
 
     for {
       row <- planAndSet
       (plans, sets) = row.unzip
-    } yield printlnSet(sets)
+      s <- printlnSet(sets)
+    } yield s
   }
 
 }
---
+++ f/f07/src/main/scala/f07/CountSet1.scala
@@ -0,0 +1,7 @@
+package f07
+
+import scala.collection.mutable.HashMap
+import collection.immutable.{List => SList}
+
+object a{
+
