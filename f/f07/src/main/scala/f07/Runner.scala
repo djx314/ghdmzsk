1d4cd4c9912e07a36ee2c4101151631a8299abe9
djx314 <djx314@sina.cn>
2022-01-28 17:41:07 +0800

    接口比肉体nerotnnt5n4人与人同依然头人

--- f/f07/src/main/scala/f07/Runner.scala
+++ f/f07/src/main/scala/f07/Runner.scala
@@ -68,6 +68,106 @@ object Runner {
     countSets.filterNot(s => CountSets.sum.exists(t => t.set == s._2)).map(_._1)
   }
 
+  def printlnSingleResult(): Unit = {
+    try {
+      val sets     = col.map(s => (s._1, s._2.mkString("|")))
+      val leftSets = CountSets.sum.filter(s => sets.forall(t => t._2 != s.set))
+      for {
+        eachSet   <- leftSets
+        setsCount <- SetsCol.setsCol
+      } {
+        setsCount match {
+          case CommonSetsList(key, firstStart, secondStart, value) =>
+            val list = for {
+              i1 <- eachSet.firstStart to 20
+              i2 <- eachSet.secondStart to 20
+            } yield {
+              val v1 =
+                try Option(value(1, i2))
+                catch {
+                  case e: Throwable => Option.empty
+                }
+              val str1 = s"$i1,$i2,${v1.getOrElse("unlimited")}"
+
+              val v2 =
+                try Option(value(i1, 1))
+                catch {
+                  case e: Throwable => Option.empty
+                }
+              val str2 = s"$i1,$i2,${v2.getOrElse("unlimited")}"
+
+              val v3 =
+                try Option(value(1, 1))
+                catch {
+                  case e: Throwable => Option.empty
+                }
+              val str3 = s"$i1,$i2,${v3.getOrElse("unlimited")}"
+
+              (str1, str2, str3)
+            }
+
+            val (listStr1, listStr2, listStr3) = list.unzip3
+            if (eachSet.set == listStr1.mkString("|")) {
+              println(s"可立刻替换的映射：firstStart:${eachSet.firstStart}, secondStart: ${eachSet.secondStart}, i1 = 1, i2 = i2, mappingKey: $key")
+              throw new Exception
+            }
+            if (eachSet.set == listStr2.mkString("|")) {
+              println(s"可立刻替换的映射：firstStart:${eachSet.firstStart}, secondStart: ${eachSet.secondStart}, i1 = i1, i2 = 1, mappingKey: $key")
+              throw new Exception
+            }
+            if (eachSet.set == listStr3.mkString("|")) {
+              println(s"可立刻替换的映射：firstStart:${eachSet.firstStart}, secondStart: ${eachSet.secondStart}, i1 = 1, i2 = 1, mappingKey: $key")
+              throw new Exception
+            }
+          case OptSetsList(key, firstStart, secondStart, value) =>
+            val list = for {
+              i1 <- eachSet.firstStart to 20
+              i2 <- eachSet.secondStart to 20
+            } yield {
+              val v1 =
+                try Option(value(1, i2))
+                catch {
+                  case e: Throwable => Option.empty
+                }
+              val str1 = s"$i1,$i2,${v1.flatten.getOrElse("unlimited")}"
+
+              val v2 =
+                try Option(value(i1, 1))
+                catch {
+                  case e: Throwable => Option.empty
+                }
+              val str2 = s"$i1,$i2,${v2.flatten.getOrElse("unlimited")}"
+
+              val v3 =
+                try Option(value(1, 1))
+                catch {
+                  case e: Throwable => Option.empty
+                }
+              val str3 = s"$i1,$i2,${v3.flatten.getOrElse("unlimited")}"
+
+              (str1, str2, str3)
+            }
+
+            val (listStr1, listStr2, listStr3) = list.unzip3
+            if (eachSet.set == listStr1.mkString("|")) {
+              println(s"可立刻替换的映射：firstStart:${eachSet.firstStart}, secondStart: ${eachSet.secondStart}, i1 = 1, i2 = i2, mappingKey: $key")
+              throw new Exception
+            }
+            if (eachSet.set == listStr2.mkString("|")) {
+              println(s"可立刻替换的映射：firstStart:${eachSet.firstStart}, secondStart: ${eachSet.secondStart}, i1 = i1, i2 = 1, mappingKey: $key")
+              throw new Exception
+            }
+            if (eachSet.set == listStr3.mkString("|")) {
+              println(s"可立刻替换的映射：firstStart:${eachSet.firstStart}, secondStart: ${eachSet.secondStart}, i1 = 1, i2 = 1, mappingKey: $key")
+              throw new Exception
+            }
+        }
+      }
+    } catch {
+      case e: Throwable =>
+    }
+  }
+
   def main(arr: Array[String]): Unit = {
     println(s"重复的映射：${distinctRunner()}")
     println(s"结果集总数：${CountSets.sum.size}")
@@ -76,7 +176,9 @@ object Runner {
     println(s"无效的映射 key：${colLeftover()}")
     println(s"重复的映射 key：${SetsCol.setsCol.map(_.key).groupBy(identity).filter(_._2.size > 1).map(_._1)}")
 
-    println(Confirm.confirm.mkString("\n"))
+    printlnSingleResult()
+
+    // println(Confirm.confirm.mkString("\n"))
     // Gen3.genRunner()
   }
 
