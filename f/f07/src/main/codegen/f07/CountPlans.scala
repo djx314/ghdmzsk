df7c849bd236cfe8e0a673e4f0f4f02621606c94
djx314 <djx314@sina.cn>
2022-03-01 15:00:03 +0800

    m

--- f/f07/src/main/codegen/f07/CountPlans.scala
+++ f/f07/src/main/codegen/f07/CountPlans.scala
@@ -1,5 +1,5 @@
 package f07
 import f07.codegen.impl._
 object CountPlans {
-  val sum: List[CountPlan] = (CountPlanSums0.sum ::: CountPlanSums1.sum ::: CountPlanSums2.sum ::: CountPlanSums3.sum)
+  val sum: List[CountPlan] = (CountPlanSums0.sum ::: CountPlanSums1.sum ::: CountPlanSums2.sum ::: CountPlanSums3.sum ::: CountPlanSums4.sum ::: CountPlanSums5.sum ::: CountPlanSums6.sum ::: CountPlanSums7.sum ::: CountPlanSums8.sum)
 }
