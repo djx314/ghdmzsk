720a370e3092b55670ef4adb47a5d92f7e8a8d9c
djx314 <djx314@sina.cn>
2022-03-23 07:32:15 +0800

    1

--- a/a03/src/main/scala/a04/Counter.scala
+++ a/a03/src/main/scala/a04/Counter.scala
@@ -3,4 +3,4 @@ package a04
 trait Number1
 case class Number1S(tail1: () => Number1, tail2: () => Number1) extends Number1
 case class Number1T(tail: () => Number1)                        extends Number1
-case class Number1U(tail: () => Number1)                        extends Number1
+case class Number1U(tail1: () => Number1, tail2: () => Number1) extends Number1
--- a/a03/src/main/scala/a04/Runner.scala
+++ a/a03/src/main/scala/a04/Runner.scala
@@ -4,22 +4,90 @@ object Runner {
 
   lazy val number1: Number1 =
     Number1S(() => Number1S(() => Number1S(() => number1Zero, () => number_2_1), () => number_2_2), () => number_2_3)
-  lazy val number1Zero: Number1 = Number1T(() => number1)
-  lazy val number1Each: Number1 = Number1T(() => number1)
+  lazy val number1Zero: Number1 = Number1U(() => number_2_3, () => number1)
 
   lazy val number_2_1: Number1 =
-    Number1S(() => Number1S(() => Number1S(() => number_2_1_Zero, () => number_2_1_Each), () => number_2_1_Each), () => number_2_1_Each)
-  lazy val number_2_1_Zero: Number1 = Number1T(() => number_2_1)
-  lazy val number_2_1_Each: Number1 = Number1T(() => number_2_1)
+    Number1S(() => Number1S(() => Number1S(() => number_2_1_Zero, () => number_3_1_1), () => number_3_1_2), () => number_3_1_3)
+  lazy val number_2_1_Zero: Number1 = Number1U(() => number_3_1_1, () => number_2_1)
 
   lazy val number_2_2: Number1 =
-    Number1S(() => Number1S(() => Number1S(() => number_2_2_Zero, () => number_2_2_Each), () => number_2_2_Each), () => number_2_2_Each)
-  lazy val number_2_2_Zero: Number1 = Number1T(() => number_2_1)
-  lazy val number_2_2_Each: Number1 = Number1T(() => number_2_1)
+    Number1S(() => Number1S(() => Number1S(() => number_2_2_Zero, () => number_3_2_1), () => number_3_2_2), () => number_3_2_3)
+  lazy val number_2_2_Zero: Number1 = Number1U(() => number_2_1, () => number_2_2)
 
   lazy val number_2_3: Number1 =
-    Number1S(() => Number1S(() => Number1S(() => number_2_3_Zero, () => number_2_3_Each), () => number_2_3_Each), () => number_2_3_Each)
-  lazy val number_2_3_Zero: Number1 = Number1T(() => number_2_3)
-  lazy val number_2_3_Each: Number1 = Number1T(() => number_2_3)
+    Number1S(() => Number1S(() => Number1S(() => number_2_3_Zero, () => number_3_3_1), () => number_3_3_2), () => number_3_3_3)
+  lazy val number_2_3_Zero: Number1 = Number1U(() => number_2_2, () => number_2_3)
+
+  lazy val number_3_1_1: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_1_1_Zero, () => number_3_1_1_Each), () => number_3_1_1_Each),
+      () => number_3_1_1_Each
+    )
+  lazy val number_3_1_1_Zero: Number1 = Number1U(() => number_2_2 /* */, () => number_3_1_1)
+  lazy val number_3_1_1_Each: Number1 = Number1T(() => number_3_1_1)
+
+  lazy val number_3_1_2: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_1_2_Zero, () => number_3_1_2_Each), () => number_3_1_2_Each),
+      () => number_3_1_2_Each
+    )
+  lazy val number_3_1_2_Zero: Number1 = Number1U(() => number_3_1_1, () => number_3_1_2)
+  lazy val number_3_1_2_Each: Number1 = Number1T(() => number_3_1_2)
+
+  lazy val number_3_1_3: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_1_3_Zero, () => number_3_1_3_Each), () => number_3_1_3_Each),
+      () => number_3_1_3_Each
+    )
+  lazy val number_3_1_3_Zero: Number1 = Number1U(() => number_3_1_2, () => number_3_1_3)
+  lazy val number_3_1_3_Each: Number1 = Number1T(() => number_3_1_3)
+
+  lazy val number_3_2_1: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_2_1_Zero, () => number_3_2_1_Each), () => number_3_2_1_Each),
+      () => number_3_2_1_Each
+    )
+  lazy val number_3_2_1_Zero: Number1 = Number1U(() => number_3_1_3, () => number_3_2_1)
+  lazy val number_3_2_1_Each: Number1 = Number1T(() => number_3_2_1)
+
+  lazy val number_3_2_2: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_2_2_Zero, () => number_3_2_2_Each), () => number_3_2_2_Each),
+      () => number_3_2_2_Each
+    )
+  lazy val number_3_2_2_Zero: Number1 = Number1U(() => number_3_2_1, () => number_3_2_2)
+  lazy val number_3_2_2_Each: Number1 = Number1T(() => number_3_2_2)
+
+  lazy val number_3_2_3: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_2_3_Zero, () => number_3_2_3_Each), () => number_3_2_3_Each),
+      () => number_3_2_3_Each
+    )
+  lazy val number_3_2_3_Zero: Number1 = Number1U(() => number_3_2_2, () => number_3_2_3)
+  lazy val number_3_2_3_Each: Number1 = Number1T(() => number_3_2_3)
+
+  lazy val number_3_3_1: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_3_1_Zero, () => number_3_3_1_Each), () => number_3_3_1_Each),
+      () => number_3_3_1_Each
+    )
+  lazy val number_3_3_1_Zero: Number1 = Number1U(() => number_3_2_3, () => number_3_3_1)
+  lazy val number_3_3_1_Each: Number1 = Number1T(() => number_3_3_1)
+
+  lazy val number_3_3_2: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_3_2_Zero, () => number_3_3_2_Each), () => number_3_3_2_Each),
+      () => number_3_3_2_Each
+    )
+  lazy val number_3_3_2_Zero: Number1 = Number1U(() => number_3_3_1, () => number_3_3_2)
+  lazy val number_3_3_2_Each: Number1 = Number1T(() => number_3_3_2)
+
+  lazy val number_3_3_3: Number1 =
+    Number1S(
+      () => Number1S(() => Number1S(() => number_3_3_3_Zero, () => number_3_3_3_Each), () => number_3_3_3_Each),
+      () => number_3_3_3_Each
+    )
+  lazy val number_3_3_3_Zero: Number1 = Number1U(() => number_3_3_2, () => number_3_3_3)
+  lazy val number_3_3_3_Each: Number1 = Number1T(() => number_3_3_3)
 
 }
