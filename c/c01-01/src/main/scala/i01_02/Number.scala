99ed0d9c2e8b6d441a4431aa5be24fbe55946b3f
djx314 <djx314@sina.cn>
2022-04-30 22:59:10 +0800

    Pythagoras

--- c/c01-01/src/main/scala/i01_02/Number.scala
+++ c/c01-01/src/main/scala/i01_02/Number.scala
@@ -1,35 +1,25 @@
 package i01_02
 
-import scala.io.Source
-import scala.util.Using
-
 trait Number1_1 {
-  def method1(num2: Number2): Number3_2
+  def method1(num2: Number2, deep: Int): Number3_2
 }
 case class Number1_1S(text: String, tail2: Number1_2) extends Number1_1 {
-  override def method1(num2: Number2): Number3_2 = num2.method3(tail2, text)
+  override def method1(num2: Number2, deep: Int): Number3_2 = num2.method3(tail2, text, deep)
 }
 
 trait Number1_2 {
-  def method2(num2: Number2): Number3_2
+  def method2(num2: Number2, deep: Int): Number3_2
 }
 case class Number1_2S(tail2: Number1_2, tail1: Number1_1) extends Number1_2 {
-  override def method2(num2: Number2): Number3_2 = Number3_2S(tail2.method2(num2), tail1.method1(num2))
-}
-case class Number1_2T(text: String) extends Number1_2 {
-  override def method2(num2: Number2): Number3_2 = Number3_2U(text)
+  override def method2(num2: Number2, deep: Int): Number3_2 = Number3_2S(tail2.method2(num2, deep), tail1.method1(num2, deep))
 }
 
 // ====
 trait Number2 {
-  def method3(num1: Number1_2, text: String): Number3_2
+  def method3(num1: Number1_2, text: String, deep: Int): Number3_2
 }
-case class Number2S(tail: Number2) extends Number2 {
-  override def method3(num1: Number1_2, text: String): Number3_2 =
-    Number3_2T(s"\n<$text>", Number3_1S(s"\n</$text>", tail.method3(num1, text)))
-}
-case class Number2T(tail: () => Number2) extends Number2 {
-  override def method3(num1: Number1_2, text: String): Number3_2 = num1.method2(tail())
+case class Number2S(tail: () => Number2) extends Number2 {
+  override def method3(num1: Number1_2, text: String, deep: Int): Number3_2 = num1.method2(tail(), deep + 1)
 }
 
 // ====
@@ -37,8 +27,7 @@ trait Number3_1 {
   def text1: String
 }
 case class Number3_1S(text: String, tail: Number3_2) extends Number3_1 {
-  def text1: String =
-    Using.resource(Source.fromString(tail.text2))(t => t.getLines().to(List).map(r => "  " + r).mkString("\n")) + text
+  def text1: String = tail.text2 + text
 }
 
 trait Number3_2 {
--- c/c01-01/src/main/scala/i01_02/Runner.scala
+++ c/c01-01/src/main/scala/i01_02/Runner.scala
@@ -2,11 +2,40 @@ package i01_02
 
 object Runner {
 
-  lazy val yaya1: Number2     = Number2S(yaya1Zero)
-  lazy val yaya1Zero: Number2 = Number2T(() => yaya1)
+  private def textBlank(deep: Int): String = " " * (deep * 2)
 
-  val emptyText: Number1_2         = Number1_2T("")
-  def text(str: String): Number1_2 = if (str.isEmpty) emptyText else Number1_2T(s"\n$str")
+  val xmlPrefix = "<"
+  val xmlSuffix = ">"
+
+  case class Number2T(tail: Number2) extends Number2 {
+    override def method3(num1: Number1_2, text: String, deep: Int): Number3_2 = {
+      val prefix = if (deep > 0) s"\n${textBlank(deep)}$xmlPrefix" else xmlPrefix
+      Number3_2T(prefix, Number3_1S(xmlSuffix, tail.method3(num1, text, deep)))
+    }
+  }
+  case class Number2U(tail: Number2) extends Number2 {
+    override def method3(num1: Number1_2, text: String, deep: Int): Number3_2 =
+      Number3_2T(text, Number3_1S(text, tail.method3(num1, text, deep)))
+  }
+  case class Number2V(tail: Number2) extends Number2 {
+    override def method3(num1: Number1_2, text: String, deep: Int): Number3_2 = {
+      val suffix = s"\n${textBlank(deep)}$xmlPrefix/"
+      Number3_2T(xmlSuffix, Number3_1S(suffix, tail.method3(num1, text, deep)))
+    }
+  }
+
+  case class Number1_2T(text: String) extends Number1_2 {
+    override def method2(num2: Number2, deep: Int): Number3_2 = Number3_2U(s"\n${textBlank(deep + 1)}$text")
+  }
+  case object Number1_2U extends Number1_2 {
+    override def method2(num2: Number2, deep: Int): Number3_2 = Number3_2U("")
+  }
+
+  lazy val yaya1: Number2     = Number2T(Number2U(Number2V(yaya1Zero)))
+  lazy val yaya1Zero: Number2 = Number2S(() => yaya1)
+
+  val emptyText: Number1_2         = Number1_2U
+  def text(str: String): Number1_2 = if (str.isEmpty) emptyText else Number1_2T(str)
 
   val num1 = Number1_1S(
     "html",
@@ -26,7 +55,7 @@ object Runner {
   )
 
   def main(arr: Array[String]): Unit = {
-    println(num1.method1(yaya1).text2)
+    println(num1.method1(yaya1, 0).text2)
   }
 
 }
