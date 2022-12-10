c82ba32c376e2e3a38beb5b9c8da800e0414d26c
djx314 <djx314@sina.cn>
2022-12-10 10:33:36 +0800

    positive positive positive python pythagoras.

--- h/h01/src/main/scala/h01/CounterAA.scala
+++ h/h01/src/main/scala/h01/CounterAA.scala
@@ -4,6 +4,14 @@ trait Number2 extends ((() => Number2) => Number2)
 
 object Number2 {
 
+  var 当前状态: Int = null.asInstanceOf[Int]
+  def 更新状态: Unit = this.synchronized {
+    当前状态 = 当前状态 + 1
+  }
+  def 重置状态: Unit = this.synchronized {
+    当前状态 = 0
+  }
+
   val S: Number2 = (tail: () => Number2) =>
     (number: () => Number2) => {
       def tail1   = tail()
@@ -32,6 +40,9 @@ object Number2 {
       number1(() => tail1)
     }
 
-  val Append: Number2 = (v: () => Number2) => Append(() => v())
+  val Append: Number2 = (v: () => Number2) => {
+    更新状态
+    Append(() => v())
+  }
 
 }
